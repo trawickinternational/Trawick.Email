40672858ae8c92457be042b8f275da3d016b73e6
sdobbins76 <scott@dobbinsweb.com>
2018-10-18 11:51:46 -0500

    ASDF

---
+++ Trawick.Email/EmailHelpers/PortalContact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Trawick.Common.Email;
+
+namespace Trawick.Email.EmailHelpers
+{
+    public class PortalContact
+    {
+        public static Trawick.Common.Email.EmailResponse SendPortalContact(EmailArgs args)
+        {
+            var response = new Trawick.Common.Email.EmailResponse() { };
+            response = Trawick.Common.Email.EmailFactory.GetEmailFactory().SendMail(args);
+            return response;
+        }
+    }
+}
